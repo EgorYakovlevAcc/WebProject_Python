84f15f767ccd8e946db995d1cf6156a125a4c924
Egor YakovlevAcc <yakovlev.es@phystech.edu>
2019-05-23 08:42:40 +0300

    all pages ready

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:root@localhost/flaskblog'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/flaskblog'
--- view.py
+++ view.py
@@ -5,3 +5,17 @@ from flask import Flask, render_template
 def index():
     return render_template('index.html')
 
+
+@app.route('/signup')
+def sign_up():
+    return render_template('sign_up.html')
+
+
+@app.route('/signin')
+def sign_in():
+    return render_template('sign_in.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
